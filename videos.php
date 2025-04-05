c784a710a95cd011b73472585413329894521944
MrSaad <mrbananatyphon2020@gmail.com>
2025-04-06 01:24:41 +0330

    Update videos.php

--- videos.php
+++ videos.php
@@ -1,14 +1,1 @@
-<?php
-$videos = [
-    "VID_20250109_172317.mp4",
-    "VID_20250109_172403.mp4",
-    // ویدیوهای بیشتر را به این آرایه اضافه کنید
-];
 
-foreach ($videos as $video) {
-    echo '<div class="reel">';
-    echo '<video src="' . $video . '" muted></video>';
-    echo '<div class="play-button">▶</div>';
-    echo '</div>';
-}
-?>
